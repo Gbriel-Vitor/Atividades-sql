e84311451fac507f5036c152f0051f8500bdd7ca
Gabriel Vitor <gabrielgonc1080@hotmail.com>
2023-07-04 14:03:45 -0300

    Exercícios plsql
    PLSQL

---
+++ Seção+9+-+Prática+Aula+6.sql
@@ -0,0 +1,23 @@
+-- Seção 9 - Estruturas de Controle 
+--
+-- Aula 6 - WHILE LOOP
+--
+
+-- WHILE LOOP
+
+SET SERVEROUTPUT ON
+ACCEPT pLimite PROMPT 'Digite o valor do limite: '
+DECLARE
+  vNumero  NUMBER(38) :=  1;
+  vLimite  NUMBER(38) := &pLimite;
+  
+  -- variaveis ja iniciadas
+BEGIN
+
+-- Variaveis inicializadas na seção Declare
+
+WHILE  vNumero <= vLimite LOOP
+    DBMS_OUTPUT.PUT_LINE('Número =  ' || to_char(vNumero));
+    vNumero := vNUmero + 1;
+END LOOP;
+END;
